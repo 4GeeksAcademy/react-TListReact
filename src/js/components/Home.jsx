32c33e15ee9cda79c0daac6fa9a202f5d976b93b
DaniVera-tech <daniss.vera@gmail.com>
2025-04-04 09:52:45 +0000

    thirt commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,19 +3,19 @@ import React, { useState } from 'react';
 const Home = () => {
     const [inputValue, setInputValue] = useState("");
     const [Mistareas, setMistareas] = useState([]);
-    
+
     return (
         <div className="container">
             <h1>Mis tareas </h1>
             <ul>
                 <li>
-                    <input 
+                    <input
                         type="text"
                         onChange={(e) => setInputValue(e.target.value)}
                         value={inputValue}
                         onKeyDown={(e) => {
                             console.log(e.key);
-                            if (e.key === "Enter") { 
+                            if (e.key === "Enter") {
                                 console.log("Enter was pressed");
                                 setMistareas(Mistareas.concat(inputValue));
                                 setInputValue("");
@@ -27,16 +27,16 @@ const Home = () => {
                 {Mistareas.map((item, index) => (
                     <li key={index}>
                         {item} {""}
-                        <i 
-                            className="fa-solid fa-trash" 
+                        <button
+                            className="fa-solid fa-trash"
                             onClick={() =>
                                 setMistareas(
                                     Mistareas.filter(
                                         (_, currentIndex) => index !== currentIndex
                                     )
-                                )   
+                                )
                             }
-                        ></i>
+                        >eliminar</button>
                     </li>
                 ))}
             </ul>
